27c4ca6b8fa472d5aac00a0cc93aa3070d488fba
Chuvetharani <pmuchuvetha@gmail.com>
2021-01-24 23:31:42 +0530

    project

---
+++ src/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'linear-gradient(darkblue, darkred), -webkit-background-clip: text, -webkit-text-fill-color: transparent',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  [theme.breakpoints.down('sm')]: {
+    mainContainer: {
+      flexDirection: "column-reverse"
+    }
+  }
+}));
